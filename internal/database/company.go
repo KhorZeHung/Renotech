75ad565285f0a30122fc1650ea9d74610c729fa0
zehung1 <zehung@lyjevents.com>
2025-10-07 08:34:48 +0800

    login return user secure

--- internal/database/company.go
+++ internal/database/company.go
@@ -7,20 +7,21 @@ import (
 )
 
 type Company struct {
-	ID                  *primitive.ObjectID  `bson:"_id,omitempty" json:"_id,omitempty"`
-	Name                string               `bson:"name" json:"name"`
-	ClientDisplayName   string               `bson:"clientDisplayName" json:"clientDisplayName"`
-	SupplierDisplayName string               `bson:"supplierDisplayName" json:"supplierDisplayName"`
-	Address             string               `bson:"address" json:"address"`
-	Website             string               `bson:"website" json:"website"`
-	Owner               *primitive.ObjectID  `bson:"owner,omitempty" json:"owner,omitempty"`
-	Logo                string               `bson:"logo" json:"logo"`
-	Contact             string               `bson:"contact" json:"contact"`
-	TermCondition       []string             `bson:"termCondition" json:"termCondition"`
-	IsDeleted           bool                 `bson:"isDeleted" json:"isDeleted"`
-	IsEnabled           bool                 `bson:"isEnabled" json:"isEnabled"`
-	CreatedAt           time.Time            `bson:"createdAt" json:"createdAt"`
-	CreatedBy           *primitive.ObjectID  `bson:"createdBy,omitempty" json:"createdBy,omitempty"`
-	UpdatedAt           time.Time            `bson:"updatedAt" json:"updatedAt"`
-	UpdatedBy           *primitive.ObjectID  `bson:"updatedBy,omitempty" json:"updatedBy,omitempty"`
+	ID                  *primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
+	Name                string              `bson:"name" json:"name"`
+	ClientDisplayName   string              `bson:"clientDisplayName" json:"clientDisplayName"`
+	SupplierDisplayName string              `bson:"supplierDisplayName" json:"supplierDisplayName"`
+	Address             string              `bson:"address" json:"address"`
+	Website             string              `bson:"website" json:"website"`
+	Owner               *primitive.ObjectID `bson:"owner,omitempty" json:"owner,omitempty"`
+	Logo                string              `bson:"logo" json:"logo"`
+	RegistrationNo      string              `bson:"registrationNo" json:"registrationNo"`
+	Contact             string              `bson:"contact" json:"contact"`
+	TermCondition       []string            `bson:"termCondition" json:"termCondition"`
+	IsDeleted           bool                `bson:"isDeleted" json:"isDeleted"`
+	IsEnabled           bool                `bson:"isEnabled" json:"isEnabled"`
+	CreatedAt           time.Time           `bson:"createdAt" json:"createdAt"`
+	CreatedBy           *primitive.ObjectID `bson:"createdBy,omitempty" json:"createdBy,omitempty"`
+	UpdatedAt           time.Time           `bson:"updatedAt" json:"updatedAt"`
+	UpdatedBy           *primitive.ObjectID `bson:"updatedBy,omitempty" json:"updatedBy,omitempty"`
 }
--- internal/model/auth.go
+++ internal/model/auth.go
@@ -12,7 +12,8 @@ type LoginRequest struct {
 }
 
 type LoginResponse struct {
-	Token string `json:"token"`
+	Token string              `json:"token"`
+	User  database.UserSecure `json:"user"`
 }
 
 type JWTClaims struct {
